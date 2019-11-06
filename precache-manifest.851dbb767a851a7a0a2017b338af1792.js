6df944b6237a4fd5362e6ed4fe7288374c2c49eb
codevickk <victoradeniji24@gmail.com>
2019-11-06 02:54:38 +0100

    Updates

---
+++ precache-manifest.851dbb767a851a7a0a2017b338af1792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d130b9d68fc54ffeee07249b29c36e12",
+    "url": "/contact-manager-react/index.html"
+  },
+  {
+    "revision": "28e5bbfea24758c0ab7a",
+    "url": "/contact-manager-react/static/css/main.b625b30f.chunk.css"
+  },
+  {
+    "revision": "76a4e905cda5452c57aa",
+    "url": "/contact-manager-react/static/js/2.67db59a8.chunk.js"
+  },
+  {
+    "revision": "28e5bbfea24758c0ab7a",
+    "url": "/contact-manager-react/static/js/main.2e226964.chunk.js"
+  },
+  {
+    "revision": "fd9bf81b322a7a093854",
+    "url": "/contact-manager-react/static/js/runtime-main.68fd63bb.js"
+  }
+]);
